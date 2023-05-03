3fb5aa66cd0ef01ea2ea4cc35c2a0f52e84533cf
azimgunes <azimgunesofficial@gmail.com>
2023-05-03 22:48:12 +0300

    User Location Updated.

--- Plame/MapVC.swift
+++ Plame/MapVC.swift
@@ -8,25 +8,38 @@
 import UIKit
 import MapKit
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
 
     
     @IBOutlet weak var mapView: MKMapView!
     
-    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         
         let backImage = UIImageView(frame: UIScreen.main.bounds)
         backImage.image = UIImage(named: "page")
         backImage.contentMode = UIView.ContentMode.scaleAspectFill
         self.view.insertSubview(backImage, at: 0)
         // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //locationManager.stopUpdatingLocation()
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.010, longitudeDelta: 0.0010)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 
     @IBAction func savePlaceButton(_ sender: Any) {
     }
